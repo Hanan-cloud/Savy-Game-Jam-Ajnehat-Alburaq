1b12b18bd8eb849b89aa7c9405dbbd8c171a42d8
Hanan-cloud <79918951+Hanan-cloud@users.noreply.github.com>
2022-05-12 18:08:25 +0300

    Working Main Menu + folders
    script to control the UI

---
+++ Savy GameJam/Assets/Script/Menu things/MainMenu.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    [SerializeField] GameObject mainMenu;
+    [SerializeField] GameObject instruction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        openMain();
+    }
+
+
+    
+
+    public void openMain()
+    {
+
+        mainMenu.SetActive(true);
+        instruction.SetActive(false);
+    }  
+    
+    public void openInstruction()
+    {
+
+        mainMenu.SetActive(false);
+        instruction.SetActive(true);
+    }   
+   public void startPlay()
+    {
+
+        SceneManager.LoadScene("Game");
+    }  
+}
